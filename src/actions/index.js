dc64f837d11befc6004f85c1458164661c0cc05a
MazzeiFacundo <facumazzei@hotmail.com>
2022-07-27 23:08:34 -0500

    secondsecond

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function sendText(text) {
     return async function (dispatch) {
-        var json = await axios.get(`http://localhost:3001/iecho/iecho?text=${text}`);
+        var json = await axios.get(`https://cwr-facundo-mazzei-challenge.herokuapp.com/iecho/iecho?text=${text}`);
         return dispatch({
             type: "GET_RESULTS",
             payload: json.data
